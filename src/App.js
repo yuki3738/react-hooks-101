ec8edd7210f2ed27b37dbfe7c0bc44827b3e5a35
yuki3738 <y.minamiya@gmail.com>
2021-03-22 00:13:48 +0900

    Implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-
-  const reset = () => setCount(0);
-  const two = () => setCount((previousCount) => previousCount - 1);
-  const divide3 = () =>
-    // setCount((previousCount) => {
-    //   if (previousCount % 3 === 0) {
-    //     return previousCount / 3;
-    //   } else {
-    //     return previousCount;
-    //   }
-    // });
-    // setCount((previousCount) => {
-    //   return previousCount % 3 === 0 ? previousCount / 3 : previousCount;
-    // });
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={two}>x2</button>
-        <button onClick={divide3}>divide3</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
-  );
+  )
 };
+
+App.defaultProps = {
+  name: '',
+  price: 1000
+}
 
 export default App;
